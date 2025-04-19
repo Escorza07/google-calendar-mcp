d9c0fa02164b65355475c7b902eed7869592de2b
Brayan Escorza <bescorza@unitru.edu.pe>
2025-04-19 15:51:06 -0500

    update files

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Google Calendar MCP Configuration
-export GOOGLE_CLIENT_ID="82122316308-u1crfqtd2urnra0pbjqlgo8ij2lbdstb.apps.googleusercontent.com"
-export GOOGLE_CLIENT_SECRET="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"
+export GOOGLE_CLIENT_ID=""
+export GOOGLE_CLIENT_SECRET=""
 export GOOGLE_REDIRECT_URI="http://localhost:3000/oauth2callback"
 
 # Aquí debes agregar tu REFRESH_TOKEN después de obtenerlo
